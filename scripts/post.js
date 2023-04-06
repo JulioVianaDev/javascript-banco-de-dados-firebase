7f8ada0e204c7da53f528460ec304f6ead65e8c7
JulioVianaDev <JulioVianaDev@gmail.com>
2023-04-06 17:03:28 -0300

    POST FUNCIONANDO

--- scripts/post.js
+++ scripts/post.js
@@ -1,3 +1,26 @@
 function sendData(){
-  console.log("enviei")
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+
+  const data ={
+    name: name,
+    email: email,
+  }
+  // fetc(url,{ argunto })
+  // metodos
+  // POST envia os dados
+  // GET recebe os dados
+  // DELETE deleta os dados
+  // PUT substitui todo o objeto
+  // PATCH substui só os argumentos recebidos
+  fetch("https://crud-javascript-vanilla-default-rtdb.firebaseio.com/pessoas.json",{
+    method: "POST",
+    headers:{
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+    .then(resposta => resposta.json())
+    .then(data=>console.log(data))
+    .catch(erro=>console.error(erro))
 }
