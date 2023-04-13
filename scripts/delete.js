38fb67b4f45e83883c14e17d6f7d8e102b1dd06a
juliovianadev <juliovianadev@gmail.com>
2023-04-13 17:16:53 -0300

    delete ok

--- scripts/delete.js
+++ scripts/delete.js
@@ -1,3 +1,14 @@
+const urlDelete = "https://crud-javascript-vanilla-default-rtdb.firebaseio.com/pessoas/"
+
 function deleteData(id){
-  console.log(id)
+  fetch(`${urlDelete}${id}.json`,{
+    method: 'DELETE',
+    headers:{
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(res=>{
+      PegarDados();
+    })
+    .catch(err=>console.error("Erro ao deletar dado",err))
 }
