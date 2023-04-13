4a34c02fc268c9c45854065ccc44cbfbc3c843cd
juliovianadev <juliovianadev@gmail.com>
2023-04-13 16:46:39 -0300

    get funcionando

--- scripts/get.js
+++ scripts/get.js
@@ -8,5 +8,20 @@ async function PegarDados(){
   // console.log(resultadoConvertido);
   // console.log(array);
   // console.log(arr);
+  function gerarTableRows(array){
+    return array.map((item,index)=> `
+      <tr>
+        <td>${index}</td>
+        <td>${item.name}</td>
+        <td>${item.email}</td>
+        <td>${item.id}</td>
+        <td><button>Editar</button></td>
+        <td><button>Deletar</button></td>
+      </tr>
+    `).join('');
+  }
+  const tableBody = document.getElementById("tbody");
+  tableBody.innerHTML = gerarTableRows(arr);
+  // tableBody.innerHTML = "<p>vim html</p>" 
 }
 PegarDados();
