8ddad6193254453f738db5776d3d78a4e501986a
juliovianadev <juliovianadev@gmail.com>
2023-04-20 17:30:07 -0300

    bug arrumado

--- scripts/update.js
+++ scripts/update.js
@@ -34,7 +34,7 @@ async function editData(id){
         document.getElementById("submit").removeAttribute("data-id");
         document.getElementById('email').value='';
         document.getElementById('name').value='';
-        document.getElementById('submit').innerText="Cadastrar!";
+        document.getElementById('submit').innerHTML="Cadastrar!";
         editMode= document.getElementById("submit").innerHTML === "Edit Aqui!";
       })
         .catch(err=>console.log("erro ao editar",err));
