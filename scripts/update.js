3a78955fb4b57bc66f15bd02b2be1ded8c6b2bf6
juliovianadev <juliovianadev@gmail.com>
2023-04-20 17:11:50 -0300

    criando a função de edit com backend

--- scripts/update.js
+++ scripts/update.js
@@ -13,3 +13,30 @@ function EnviandoDadosAtuais(id,nameItem,emailItem){
     editMode = document.getElementById("submit").innerHTML === "Edit Aqui!";
     // console.log(editMode)
 }
+const urlEdit = "https://crud-javascript-vanilla-default-rtdb.firebaseio.com/pessoas/"
+
+async function editData(id){
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const data ={
+        name: name,
+        email: email,
+    }
+    response = await fetch(`${urlEdit}${id}.json`,{
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })  
+      .then(res=>{
+        PegarDados();
+        document.getElementById("submit").removeAttribute("data-id");
+        document.getElementById('email').value='';
+        document.getElementById('name').value='';
+        document.getElementById('submit').innerText="Cadastrar!";
+        editMode= document.getElementById("submit").innerHTML === "Edit Aqui!";
+      })
+        .catch(err=>console.log("erro ao editar",err));
+
+}
